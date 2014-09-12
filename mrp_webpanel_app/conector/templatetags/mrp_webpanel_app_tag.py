3088a9c6f2c5ca1883a30d8e79885437b03bb264
Alejandro Núñez <alejandro@toporojo.es>
2014-09-12 12:27:58 +0200

    Añadidas traduciones a tareas

--- mrp_webpanel_app/conector/templatetags/mrp_webpanel_app_tag.py
+++ mrp_webpanel_app/conector/templatetags/mrp_webpanel_app_tag.py
@@ -3,5 +3,5 @@ register = template.Library()
 
 @register.filter
 def traduccion(arg):
-    estados = {'draft': 'Borrador', 'confirmed': 'Confirmada', 'assigned': 'Lista para producir', 'ready': 'Listo para fabricar', 'in_production': 'Produccion iniciada',}
+    estados = {'draft': 'Borrador', 'open': 'Abierto', 'confirmed': 'Confirmada', 'assigned': 'Lista para producir', 'ready': 'Listo para fabricar', 'in_production': 'Produccion iniciada',}
     return estados.get(arg)
